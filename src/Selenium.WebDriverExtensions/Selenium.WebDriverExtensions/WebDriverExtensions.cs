7b37f984680248b53c8d218f9da24fccb5fb802b
another-one-employee <72085557+another-one-employee@users.noreply.github.com>
2024-11-22 17:51:00 +0300

    Add first extensions implementation

---
+++ src/Selenium.WebDriverExtensions/Selenium.WebDriverExtensions/WebDriverExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium.WebDriverExtensions
+{
+    public static class WebDriverExtensions
+    {
+        public static void MoveToElement(this IWebDriver driver, IWebElement element)
+        {
+            driver.ScrollToElement(element);
+            var actions = new Actions(driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+        }
+
+        private static void ScrollToElement(this IWebDriver driver, IWebElement element)
+        {
+            var x = element.Location.X;
+            var y = element.Location.Y;
+
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"window.scrollTo({x},{y})");
+            js.ExecuteScript($"window.scrollBy(0, -210)");
+        }
+
+        public static void ClickOnElement(this IWebDriver driver, IWebElement element)
+        {
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].click();", element);
+        }
+
+        public static void ScrollToPageStart(this IWebDriver driver)
+        {
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollTo(0, 0);");
+        }
+
+        public static void OpenLinkInNewTab(this IWebDriver driver, string link)
+        {
+            var js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.open(arguments[0], '_blank');", link);
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+
+        public static void CloseLastPage(this IWebDriver driver)
+        {
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            driver.Close();
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+
+        public static void WaitForFullPageLoad(this IWebDriver driver)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+        }
+    }
+}
---
+++ src/Selenium.WebDriverExtensions/Selenium.WebDriverExtensions/WebDriverHolder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium.WebDriverExtensions
+{
+    public static class WebDriverHolder
+    {
+        [ThreadStatic]
+        private static IWebDriver instance;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                return instance = instance ?? new ChromeDriver();
+            }
+        }
+
+        public static void InitDriver(IWebDriver driver)
+        {
+            instance = driver;
+        }
+
+        public static void Cleanup()
+        {
+            instance?.Quit();
+            instance = null;
+        }
+    }
+}
